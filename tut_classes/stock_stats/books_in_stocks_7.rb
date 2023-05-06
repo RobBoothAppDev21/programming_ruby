79c480b9a7c49cb68c3e1a3c7d5cae5e05ea4203
Rob Young <rhyoung214@gmail.com>
2023-05-06 11:38:50 -0700

    completed ch3 on classes

---
+++ tut_classes/stock_stats/books_in_stocks_7.rb
@@ -0,0 +1,21 @@
+class BookInStock
+  attr_reader :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+
+  def price_in_cents
+    (price * 100).round
+  end
+
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
+end
+
+book = BookInStock.new("isbn1", 33.80)
+puts "Price          = #{book.price}"
+puts "Price in cents = #{book.price_in_cents}"
